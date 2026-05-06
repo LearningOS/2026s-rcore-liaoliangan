36b30aacf3f4c52fcb20deae79a3484cb025c793
LA <337172372@qq.com>
2026-05-06 14:47:29 +0800

    Remove-ch7-stale-todo-markers

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -135,7 +135,6 @@ pub fn sys_kill(pid: usize, signum: i32) -> isize {
     }
 }
 
-/// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
@@ -160,7 +159,6 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     0
 }
 
-/// YOUR JOB: Implement mmap.
 pub fn sys_mmap(start: usize, len: usize, port: usize) -> isize {
     trace!("kernel:pid[{}] sys_mmap", current_task().unwrap().pid.0);
     if start % PAGE_SIZE != 0 || len == 0 || port == 0 || port & !0x7 != 0 {
@@ -191,7 +189,6 @@ pub fn sys_mmap(start: usize, len: usize, port: usize) -> isize {
     }
 }
 
-/// YOUR JOB: Implement munmap.
 pub fn sys_munmap(start: usize, len: usize) -> isize {
     trace!("kernel:pid[{}] sys_munmap", current_task().unwrap().pid.0);
     if start % PAGE_SIZE != 0 || len == 0 {
@@ -219,7 +216,6 @@ pub fn sys_sbrk(size: i32) -> isize {
     }
 }
 
-/// YOUR JOB: Implement spawn.
 /// HINT: fork + exec =/= spawn
 pub fn sys_spawn(path: *const u8) -> isize {
     trace!("kernel:pid[{}] sys_spawn", current_task().unwrap().pid.0);
@@ -239,7 +235,6 @@ pub fn sys_spawn(path: *const u8) -> isize {
     }
 }
 
-// YOUR JOB: Set task priority.
 pub fn sys_set_priority(prio: isize) -> isize {
     trace!("kernel:pid[{}] sys_set_priority", current_task().unwrap().pid.0);
     if prio <= 1 {
